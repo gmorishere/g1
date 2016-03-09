e4b5118361c065ebffd4ebd13019317ce3f3031c
Shahab <safaeishahab@yahoo.com>
2016-03-09 20:08:27 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -6,20 +6,6 @@ function run(msg, matches)
  Manager : @Amirsasan1998
  Chek Out Our Channel :
  Telegram.me/Tfbkingdom
-  
-Planlist :
-
-1⃣ Month 2$ 🤑
-2⃣ Months 3$🤑
-3⃣ Months 5$🤑
- =========================
- لیست پلن ها :
-
- 1⃣ ماه 3 تومان🤑
- 2⃣ ماه 5 تومان🤑
- 3⃣ ماه 8 تومان 🤑
-
- Ask @Ww_Shahabsaf_wW for Groups
  GNU GPL v2 license.]]
 end
 
