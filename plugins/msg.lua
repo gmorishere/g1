d34b0de16906b12caa4746273db0b5d19ce5aa07
Shahab <safaeishahab@yahoo.com>
2016-03-15 12:31:46 +0330

    Update msg.lua

--- plugins/msg.lua
+++ plugins/msg.lua
@@ -4,14 +4,14 @@ if not is_sudo(msg) then
 return nil
 end
  local user_id = msg.id
-      print('checking msg.idbyarman —--> '..user_id)
-      print('checked.the truth idbyaramn —--> '..user_id)
-      return "\n\nThispm Id(pluginbyarman):"..msg.id
+      print('checking msg.idbyShahab —--> '..user_id)
+      print('checked.the truth idbyShahab —--> '..user_id)
+      return "\n\nThispm Id(pluginbyShahab):"..msg.id
 end
 
 return {
     patterns = {
-     "(.*)"
+     "/get (.*)"
    }, 
     run = run 
 }
